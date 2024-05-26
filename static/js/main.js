7abfeefb144aa8ad926973b10148f752d7b802ac
raqger <raqgeronimo@hotmail.com>
2024-05-26 11:03:17 -0300

    git commit -m "nuevosCambios"

---
+++ static/js/main.js
@@ -0,0 +1,36 @@
+console.log("estoy en una carpeta js");
+
+
+/*let listElements = document.querySelectorAll('.list_button--click');
+listElements.forEach(listElement =>{
+    listElement.addEventListener('click', ()=> {
+
+        let height = 0;
+        let menu = listElement.nextElementSibling;
+        if(menu.clientHeight == "0"){
+            height=menu.scrollHeight;
+        }
+
+        menu.style.height = height + "px";
+    })
+});
+
+ id="myTopNav"
+let listElements = document.querySelectorAll('.list_show');
+listElements.forEach(listElement => {
+
+
+})
+
+function toggleMenu() {
+    var menu = document.getElementById("myTopNav");
+    var options = menu.querySelector("ul");
+
+    if (options.classList.contains("hidden-options")) {
+        // Mostrar opciones
+        options.classList.remove("hidden-options");
+    } else {
+        // Ocultar opciones
+        options.classList.add("hidden-options");
+    }
+}*/
